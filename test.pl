95517048bf52883ba80898d9c416eaf0627da2f3
Jing Kang - 康 小靖 <kxj@cpan.org>
2008-04-22 22:29:14 +0000

    Import of KXJ/Win32-SystemInfo-CpuUsage-0.02 from CPAN.
    gitpan-cpan-distribution: Win32-SystemInfo-CpuUsage
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         KXJ/Win32-SystemInfo-CpuUsage-0.02.tar.gz
    gitpan-cpan-author:       KXJ
    gitpan-cpan-maturity:     released

--- test.pl
+++ test.pl
@@ -4,7 +4,7 @@ my $i = 0;
 my $s = 0;
 while($i < 5){
 	$i++;
-	my $usage = Win32::SystemInfo::CpuUsage::getCpuUsage(4000);
+	my $usage = Win32::SystemInfo::CpuUsage::getCpuUsage(1000);
 	print "$i: cpu usage $usage\n";
 	$s += $usage ;
 }
